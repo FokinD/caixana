530f617c4c49cdffef7e92188b03c13e268067c8
Dmitry Fokin <fokind1@yandex.ru>
2018-03-29 13:29:49 +0300

    .

--- client/controller/App.controller.js
+++ client/controller/App.controller.js
@@ -1,15 +1,58 @@
+/* global sap $ Cookies */
+'use strict';
+
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/demo/basicTemplate/model/formatter"
-], function(Controller, formatter) {
-	"use strict";
+  'sap/ui/core/mvc/Controller',
+  'sap/ui/model/json/JSONModel',
+  'sap/m/MessageToast',
+], function(Controller, JSONModel, MessageToast) {
+  return Controller.extend('tms.basic.controller.App', {
+    onInit: function() {
+      this.getView().setModel(new JSONModel());
+    },
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+    onPressLogin: function() {
+      var oView = this.getView();
+      var oModel = oView.getModel();
+      var oResourceBundle = oView.getModel('i18n').getResourceBundle();
 
-		formatter: formatter,
+      $.post(
+        {
+          url: $.sap.formatMessage('{0}Users/login', this.getOwnerComponent().getManifestEntry('/sap.app/dataSources/api/uri')),
+          data: $.sap.formatMessage('\'{\'"username":"{0}","password":"{1}"\'}\'', [oModel.getProperty('/username'), oModel.getProperty('/password')]),
+          contentType: 'application/json',
+        })
+      .done(function(data, status, xhr) {
+        Cookies.set('AccessToken', data);
+        MessageToast.show(oResourceBundle.getText('authSuccess'));
+      })
+      .fail(function(data) {
+        Cookies.remove('AccessToken');
+        MessageToast.show(oResourceBundle.getText('authError'));
+      });
+    },
 
-		onInit: function () {
+    onPressLogout: function() {
+      var oView = this.getView();
+      var oModel = oView.getModel();
+      var oResourceBundle = oView.getModel('i18n').getResourceBundle();
+      var oAccessToken = Cookies.getJSON('AccessToken');
 
-		}
-	});
+      if (oAccessToken) {
+        $.post(
+          {
+            url: $.sap.formatMessage('{0}Users/logout', this.getOwnerComponent().getManifestEntry('/sap.app/dataSources/api/uri')),
+            contentType: 'application/json',
+            headers: {'Authorization': oAccessToken.id},
+          })
+        .done(function(data, status, xhr) {
+          MessageToast.show(oResourceBundle.getText('authLogoutSuccess'));
+        })
+        .fail(function(data) {
+          MessageToast.show(oResourceBundle.getText('authLogoutSuccess'));
+        });
+        Cookies.remove('AccessToken');
+      }
+    },
+  });
 });
